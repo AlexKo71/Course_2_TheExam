4cb96bb4aa4fefc24072e5063b9e7b23061bc98b
Alexey Kolodin <alekko-sp@yandex.ru>
2023-06-10 20:11:40 +0300

    there is an empty branch here

---
+++ src/main/java/pro/sky/course_2_theexam/Course2TheExamApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.course_2_theexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course2TheExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Course2TheExamApplication.class, args);
+    }
+
+}
